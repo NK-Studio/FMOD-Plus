e76eba3075bc69a045c2df0a59d2eedd06991f92
NK Studio <NK-Studio@users.noreply.github.com>
2023-07-20 10:42:45 +0900

    박스 그룹 GUID 변경

--- Editor/FMODParameterSenderEditor.cs
+++ Editor/FMODParameterSenderEditor.cs
@@ -44,7 +44,7 @@ namespace FMODPlus
             MonoScript studioListener = AssetDatabase.LoadAssetAtPath<MonoScript>(path);
             FMODIconEditor.ApplyIcon(darkIcon, whiteIcon, studioListener);
 
-            string boxGroupStyleSheetPath = AssetDatabase.GUIDToAssetPath("6a25e899d15eb994b85241dddfd90559");
+            string boxGroupStyleSheetPath = AssetDatabase.GUIDToAssetPath("5600a59cbafd24acf808fa415167310e");
             groupBoxStyleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(boxGroupStyleSheetPath);
 
             string buttonStyleSheetPath = AssetDatabase.GUIDToAssetPath("db197c96211fc47319d2b84dcd02aacd");
